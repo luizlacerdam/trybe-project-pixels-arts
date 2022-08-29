877593e352a55a80e26ae2cdb2b8772c8ad0c55e
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-08-29 15:17:26 -0300

    requisito-3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  let result;
+  if (!employeeName) {
+    result = {};
+  } else {
+    result = data.employees.find((element) => element.firstName === employeeName
+    || element.lastName === employeeName);
+  }
+  return result;
 }
-
+console.log(getEmployeeByName('Nigel'));
 module.exports = getEmployeeByName;
