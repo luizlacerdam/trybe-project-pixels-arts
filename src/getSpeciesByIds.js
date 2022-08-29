10710dd5ae6ff1f75199185a4498df1652bf3576
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-08-29 10:36:22 -0300

    requisito-1-2-de-3

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,8 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(ids) {
-  // seu código aqui
+  return species.filter((element) => element.id === ids);
 }
-
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 module.exports = getSpeciesByIds;
