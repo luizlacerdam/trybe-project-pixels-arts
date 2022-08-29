e6a5ac48a9b35e5578b76c39d66f09211e5ac613
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-08-29 15:02:27 -0300

    requisito-2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const encontrarEspecie = species.find((item) => item.name === animal);
+  return encontrarEspecie.residents.every((element) => element.age > age);
 }
-
+console.log(getAnimalsOlderThan('bears', 5));
 module.exports = getAnimalsOlderThan;
