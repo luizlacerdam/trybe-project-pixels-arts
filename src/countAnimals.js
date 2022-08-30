d4b933357e940aa9c2e37edd4602c74eb558790b
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-08-30 18:33:51 -0300

    requsito-5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  const especiesAnimais = data.species;
+  if (!animal) {
+    const all = especiesAnimais.reduce((acc, curr) => {
+      acc[curr.name] = curr.residents.length;
+      return acc;
+    }, {});
+    return all;
+  }
+  const { specie, sex } = animal;
+  const escontrarEspecie = especiesAnimais.find((especie) => especie.name === specie);
+  if (!sex) {
+    return escontrarEspecie.residents.length;
+  }
+  const filtroSexo = escontrarEspecie.residents.filter((resident) => resident.sex === sex);
+  return filtroSexo.length;
+  
 }
-
+console.log(countAnimals({ specie: 'giraffes', sex: 'male' }));
 module.exports = countAnimals;
