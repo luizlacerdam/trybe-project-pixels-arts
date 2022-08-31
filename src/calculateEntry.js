7505c0c97d612e58d7272e6fe66a927834ef3ebb
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-08-31 09:39:17 -0300

    requsito-8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,9 +1,23 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const child = entrants.filter((item) => item.age < 18);
+  const adult = entrants.filter((item) => item.age >= 18 && item.age < 50);
+  const senior = entrants.filter((item) => item.age >= 50);
+  return {
+    child: child.length,
+    adult: adult.length,
+    senior: senior.length,
+  };
 }
-
+console.log(countEntrants([
+	{ name:  'Lara Carvalho', age:  5 },
+	{ name:  'Frederico Moreira', age:  5 },
+	{ name:  'Pedro Henrique Carvalho', age:  5 },
+	{ name:  'Maria Costa', age:  18 },
+	{ name:  'Núbia Souza', age:  18 },
+	{ name:  'Carlos Nogueira', age:  50 },
+]));
 function calculateEntry(entrants) {
   // seu código aqui
 }
--- src/handlerElephants.js
+++ src/handlerElephants.js
@@ -32,5 +32,4 @@ const handlerElephants = (param) => {
   }
   return computeData(param, elephants);
 };
-console.log(handlerElephants('location'));
 module.exports = handlerElephants;
