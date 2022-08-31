43e4c31a8ffa9dd5d135afd4b04717475380b91c
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-08-31 09:58:55 -0300

    requsito-8-completo

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,6 +1,9 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
+  if (!entrants || Object.keys(entrants).length === 0) {
+    return 0;
+  }
   const child = entrants.filter((item) => item.age < 18);
   const adult = entrants.filter((item) => item.age >= 18 && item.age < 50);
   const senior = entrants.filter((item) => item.age >= 50);
@@ -10,16 +13,17 @@ function countEntrants(entrants) {
     senior: senior.length,
   };
 }
-console.log(countEntrants([
-	{ name:  'Lara Carvalho', age:  5 },
-	{ name:  'Frederico Moreira', age:  5 },
-	{ name:  'Pedro Henrique Carvalho', age:  5 },
-	{ name:  'Maria Costa', age:  18 },
-	{ name:  'Núbia Souza', age:  18 },
-	{ name:  'Carlos Nogueira', age:  50 },
-]));
-function calculateEntry(entrants) {
-  // seu código aqui
-}
 
+function calculateEntry(entrants) {
+  if (!entrants || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const { adult, senior, child } = data.prices;
+  const quantChild = countEntrants(entrants).child;
+  const quantAdult = countEntrants(entrants).adult;
+  const quantSenior = countEntrants(entrants).senior;
+  const total = (adult * quantAdult) + (senior * quantSenior) + (child * quantChild);
+  return total;
+}
+console.log(countEntrants({}));
 module.exports = { calculateEntry, countEntrants };
