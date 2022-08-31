cbdae3e87e9eaf649001960ffed576270067a5f5
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-08-31 18:08:45 -0300

    requsito-11-findByName

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,24 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function findByName(name) {
+  const colaboradores = data.employees;
+  const findFirstName = colaboradores.find((colaborador) => colaborador.firstName === name);
+  const findLastName = colaboradores.find((colaborador) => colaborador.lastName === name);
+  if (findFirstName !== undefined) {
+    return findFirstName;
+  }
+  if (findLastName !== undefined) {
+    return findLastName;
+  }
+  return new Error('Informações inválidas');
 }
-
+console.log(findByName('sasa'));
+// function findById(id) {
+  
+// }
+function getEmployeesCoverage(obj) {
+  const { name, id } = obj;
+  return 0;
+}
+// console.log(getEmployeesCoverage({ name: 'luiz' }));
 module.exports = getEmployeesCoverage;
