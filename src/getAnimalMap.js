830cb698573b4c78efc58287570bc3f0c238a5df
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-09-03 10:50:49 -0300

    requsito-14-findAnimals

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
-
-function getAnimalMap(options) {
-  // seu código aqui
+const { species } = data;
+function findLocations() {
+  const arr = [];
+  return species
 }
-
+console.log(findLocations());
+function findAnimals(location) {
+  
+  const filtroAnimais = species.filter((animal) => animal.location === location);
+  const arr = [];
+  filtroAnimais.forEach((item) => arr.push(item.name));
+  return arr;
+}
+console.log(findAnimals('NE'));
+function getAnimalMap(options) {
+  return species;
+}
+// console.log(getAnimalMap({}));
 module.exports = getAnimalMap;
